0ebaf26db6d648e752324918262c337bfd59fd07
byuan08 <byuan08@users.noreply.github.com>
2016-09-14 22:33:48 -0400

    Update pyqt_test.py

--- PyQt4/pyqt_test.py
+++ PyQt4/pyqt_test.py
@@ -1,2 +1,9 @@
 import sys
 from PyQt4 import QtGui
+
+app = QtGui.QAppication(sys.argv)
+window = QtGui.QWidget()
+window.setGeometry(0, 0, 500, 300)
+window.setWindowTitle("My PyWQt App")
+
+window.show()
